c622f9f546dffe467f526f6aa9727f85842801a4
kopfire <Gelud2002>
2021-03-30 01:00:21 +0400

    not work code

---
+++ src/ExchangeDevice.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class ExchangeDevice {
+
+    TreeSet<Integer> dataSet = new TreeSet<>();
+
+    public ExchangeDevice(int ... count){
+        for (int i:count){
+            dataSet.add(i);
+        }
+    }
+
+    public HashMap<Integer, Integer> exchange(int money){
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args){
+        ExchangeDevice exchangeDevice = new ExchangeDevice(14,5,5,4,5);
+    }
+}
