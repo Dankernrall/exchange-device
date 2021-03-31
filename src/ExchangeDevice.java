952cb2a38d428890c0908ff1850996f16bcb05d2
kopfire <Gelud2002>
2021-03-31 13:07:31 +0400

    ok

--- src/ExchangeDevice.java
+++ src/ExchangeDevice.java
@@ -9,9 +9,67 @@ public class ExchangeDevice {
         for (int i:count){
             dataSet.add(i);
         }
+        dataSet = (TreeSet) dataSet.descendingSet();
     }
 
-    public HashMap<Integer, Integer> exchange(int money){
+    public String exchange(int money){
+        int countMoneyWork = 0;
+        int countMoneyResult = 0;
+        int moneyWork = money;
+        HashMap<Integer, Integer> hashMapResult = new HashMap<>();
+        HashMap<Integer, Integer> hashMapWork = new HashMap<>();
+        while (dataSet.size() != 0) {
+            for (int i : dataSet) {
+                while (moneyWork - i > -1) {
+                    try {
+                        hashMapWork.put(i, hashMapWork.get(i) + 1);
+                    } catch (NullPointerException e) {
+                        hashMapWork.put(i, 1);
+                    }
+                    moneyWork -= i;
+                }
+            }
+            for(int i : hashMapWork.keySet()) {
+                if (hashMapWork.get(i) != 0) {
+                    countMoneyWork += hashMapWork.get(i) * i;
+                }
+            }
+            if (moneyWork == 0) {
+                if (hashMapResult.size() == 0) {
+                    for(int i : hashMapWork.keySet()) {
+                        hashMapResult.put(i, hashMapWork.get(i));
+                        countMoneyResult = countMoneyWork;
+                    }
+                } else if (countMoneyWork < countMoneyResult) {
+                    hashMapResult.clear();
+                    for(int i : hashMapWork.keySet()) {
+                        hashMapResult.put(i, hashMapWork.get(i));
+                    }
+                }
+            }
+            dataSet.remove(dataSet.first());
+            moneyWork = money;
+            hashMapWork.clear();
+            countMoneyWork = 0;
+        }
 
+        if (hashMapResult.size() == 0){
+            return "Невозможно получить из полученного набора введенное значение.";
+        }
+
+        String output = "";
+        for(int i : hashMapResult.keySet()) {
+            int count = hashMapResult.get(i)%100;
+            if ((count>4 & count<21) | (count%10 > 4)) {
+                output += hashMapResult.get(i) + " монет по " + i + " руб.\n";
+            }
+            else if (count%10 == 1){
+                output += hashMapResult.get(i) + " монета по " + i + " руб.\n";
+            }
+            else{
+                output += hashMapResult.get(i) + " монеты по " + i + " руб.\n";
+            }
+        }
+        return output;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-        ExchangeDevice exchangeDevice = new ExchangeDevice(14,5,5,4,5);
+        ExchangeDevice exchangeDevice = new ExchangeDevice(1,5,10,25,100);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int count = sc.nextInt();
+        System.out.println(exchangeDevice.exchange(count));
     }
 }
