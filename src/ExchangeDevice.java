f9f96608a546605e24ec7396007318bdb35e6ef5
kopfire <Gelud2002>
2021-04-01 13:28:41 +0400

    итоговый код

--- src/ExchangeDevice.java
+++ src/ExchangeDevice.java
@@ -3,56 +3,52 @@ import java.util.TreeSet;
 
 public class ExchangeDevice {
 
+    //введем список из неповторяющихся значений, который будет отсортирован для удобства
     TreeSet<Integer> dataSet = new TreeSet<>();
 
+    //в конструктор передаем Любое количество целых чисел
     public ExchangeDevice(int ... count){
+        //заполняем этими целыми числами dataSet
         for (int i:count){
             dataSet.add(i);
         }
-        dataSet = (TreeSet) dataSet.descendingSet();
+        //сортируем dataSet в обратном порядке
+        dataSet = (TreeSet<Integer>) dataSet.descendingSet();
     }
 
-    public String exchange(int money){
-        int countMoneyWork = 0;
-        int countMoneyResult = 0;
-        int moneyWork = money;
-        HashMap<Integer, Integer> hashMapResult = new HashMap<>();
-        HashMap<Integer, Integer> hashMapWork = new HashMap<>();
-        while (dataSet.size() != 0) {
-            for (int i : dataSet) {
-                for (int j : dataSet) {//dodelat
-                    while (moneyWork - i > -1) {
-                        try {
-                            hashMapWork.put(i, hashMapWork.get(i) + 1);
-                        } catch (NullPointerException e) {
-                            hashMapWork.put(i, 1);
-                        }
-                        moneyWork -= i;
+    //метод, в который передается рабочая сумма
+    public String exchange(int money) {
+        int countMoneyWork = 0; //количество монет на Данном проходе цикла
+        int countMoneyResult = Integer.MAX_VALUE; // количество монет при минимальном проходе цикла
+        int moneyWork = money; //условно делаем из money "константу"
+        HashMap<Integer, Integer> hashMapResult = new HashMap<>(); //задаем итоговую мапу монет и их количества
+        HashMap<Integer, Integer> hashMapWork = new HashMap<>(); //задаем "рабочую" мапу монет и их количества
+        while (dataSet.size() != 0) { //пока dataSet не равен нулю
+            for (int i : dataSet) { //перебираем значения dataSet, начиная с большего
+                while (moneyWork - i > -1) { //вычитаем, пока число положительное
+                    try {
+                        hashMapWork.put(i, hashMapWork.get(i) + 1); //заполняем рабочую мапу
+                    } catch (NullPointerException e) {
+                        hashMapWork.put(i, 1); //заполняем рабочую мапу
                     }
+                    moneyWork -= i; //вычитаем "по-настоящему"
                 }
             }
-            for(int i : hashMapWork.keySet()) {
-                if (hashMapWork.get(i) != 0) {
-                    countMoneyWork += hashMapWork.get(i) * i;
-                }
+            for (int i : hashMapWork.keySet()) { //узнаем текущее количество монет
+                countMoneyWork += hashMapWork.get(i); // и записываем в рабочее число
             }
-            if (moneyWork == 0) {
-                if (countMoneyResult == 0) {
-                    for(int i : hashMapWork.keySet()) {
-                        hashMapResult.put(i, hashMapWork.get(i));
-                        countMoneyResult = countMoneyWork;
-                    }
-                } else if (countMoneyWork < countMoneyResult) {
-                    hashMapResult.clear();
-                    for(int i : hashMapWork.keySet()) {
-                        hashMapResult.put(i, hashMapWork.get(i));
-                    }
+            //если в итоге у нас получилось получить "итоговое число" в этом проходе
+            if (moneyWork == 0 & countMoneyWork < countMoneyResult) {
+                hashMapResult.clear(); //очищаем прошлое заполнение мапы результата
+                for (int i : hashMapWork.keySet()) { //заполняем мапу результата рабочей мапой
+                    hashMapResult.put(i, hashMapWork.get(i));
                 }
+                countMoneyResult = countMoneyWork; //обновляем значение результата
             }
-            dataSet.remove(dataSet.first());
-            moneyWork = money;
-            hashMapWork.clear();
-            countMoneyWork = 0;
+            dataSet.remove(dataSet.first()); // удаляем первый элемент, чтобы цикл работал
+            moneyWork = money; //возвращаем исходное значение moneyWork
+            hashMapWork.clear(); //очищаем рабочую мапу
+            countMoneyWork = 0; //обнуляем рабочую переменную
         }
 
         if (hashMapResult.size() == 0){
@@ -62,15 +58,8 @@ public class ExchangeDevice {
         StringBuilder output = new StringBuilder();
         for(int i : hashMapResult.keySet()) {
             int count = hashMapResult.get(i)%100;
-            if ((count>4 & count<21) | (count%10 > 4)) {
-                output.append(hashMapResult.get(i)).append(" монет по ").append(i).append(" руб.\n");
-            }
-            else if (count%10 == 1){
-                output.append(hashMapResult.get(i)).append(" монета по ").append(i).append(" руб.\n");
-            }
-            else{
-                output.append(hashMapResult.get(i)).append(" монеты по ").append(i).append(" руб.\n");
-            }
+            //склонения
+            output.append(hashMapResult.get(i)).append(((count>4 & count<21) | (count%10 > 4))?(" монет по "):(count%10 == 1)?(" монета по "):(" монеты по ")).append(i).append(" руб.\n");
         }
         return output.toString();
     }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        ExchangeDevice exchangeDevice = new ExchangeDevice(2,5,10,25,100);
+        ExchangeDevice exchangeDevice = new ExchangeDevice(1,5,10,25,100);
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите число: ");
         int count = sc.nextInt();
