ee0f62f09b4d60d0ae582c0dbf7537809ff92efa
Никита <grand.marchello@gmail.com>
2021-04-21 15:41:02 +0400

    Исправленное решение

--- Java/src/AvailableChecker.java
+++ Java/src/AvailableChecker.java
@@ -1,29 +1,37 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class AvailableChecker {
     List<Integer> delivery = new ArrayList<>();
     int count;
+    int tmp;
+    int k = 0;
+    int[] test;
 
-    public AvailableChecker(int value, Integer... arr) {
-        Arrays.sort(arr, Collections.reverseOrder()); //Сортируем массив по убыванию,
-        for (Integer integer : arr) { // для удобства
-            count = value / integer; //Узнаем количество необходимых монет
-            if (count != 0) {
-                for (int j = 0; j < count; j++)
-                    delivery.add(integer); //Записываем в массив подходящую для размена монету
+    public void AvailableChecker(int value, int[] arr) {
+        try {
+            test = arr;
+            tmp = value;
+            for (Integer integer : arr) {
+                if (integer != 0) {
+                    count = value / integer; //Узнаем количество необходимых монет
+                    for (int j = 0; j < count; j++)
+                        delivery.add(integer); //Записываем в массив подходящую для размена монету
+                    value %= integer;
+                }
             }
-            value %= integer; //Остаток от предыдущего размена
-        }
-        if (value > 0) //Если пройдя по всем монетам, есть остаток, то размен невозможен
-            System.out.println("Банкомат не может разменять данную сумму " +
-                    "введенным номиналом монет!");
-        else {
-            System.out.println("Выдано монет:\n" + delivery
-                    .toString().replace("[", "")
-                    .replace("]", "") + ".");
+            if (value > 0) {
+                test[k] = 0;
+                k++;
+                delivery.clear();
+                AvailableChecker(tmp, test);
+            } else {
+                System.out.println("Выдано монет:\n" + delivery
+                        .toString().replace("[", "")
+                        .replace("]", "") + ".");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Невозможно разменять " + tmp + "Р набранными монетами.");
         }
     }
 }
--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        new AvailableChecker(54, 2, 5, 10);
-        //new AvailableChecker(53,2,5,10); //- Вариант для отработки
+        AvailableChecker availableChecker = new AvailableChecker();
+        int[] nums = {2, 3, 5};
+        for (int i = 0; i < nums.length / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length - i - 1];
+            nums[nums.length - i - 1] = temp;
+        }
+        availableChecker.AvailableChecker(11, nums);
+        // availableChecker.AvailableChecker(1, nums); //- Вариант для отработки
         //невозможности размена.
-        //value - Сумма для размена, arr - номиналы доступных монет для размена
+        //value - Сумма для размена, nums - номиналы доступных монет для размена
     }
 }
